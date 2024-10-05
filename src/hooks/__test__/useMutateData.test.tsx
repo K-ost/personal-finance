51c6d18af2931d8f61a35803e694a190f4ad3206
K-ost <golubkovru@gmail.com>
2024-10-05 19:30:03 +0300

    useMutateData hook tests

---
+++ src/hooks/__test__/useMutateData.test.tsx
@@ -0,0 +1,85 @@
+import { beforeAll, describe, expect, it } from "vitest";
+import { APINock, WrapperHook } from "../../tests/testUtils";
+import { BudgetFactory } from "../../tests/factories";
+import { act, renderHook, waitFor } from "@testing-library/react";
+import useMutateData from "../useMutateData";
+import { Budget } from "../../types";
+
+const mockedBudget = BudgetFactory.build();
+const bodyBudget = { ...mockedBudget };
+delete bodyBudget.id;
+delete bodyBudget.latest;
+const patchedData = {
+  ...mockedBudget,
+  category: "General",
+};
+
+describe("useMutateData", () => {
+  beforeAll(() => {
+    APINock.post("/budgets", bodyBudget).reply(200, mockedBudget);
+    APINock.patch("/budgets/1", { category: "General" }).reply(
+      200,
+      patchedData
+    );
+    APINock.delete("/budgets/1").reply(200, {});
+  });
+
+  it("Post data", async () => {
+    const { result } = renderHook(
+      () =>
+        useMutateData<Budget, Omit<Partial<Budget>, "id" | "latest">>({
+          key: ["budgets"],
+          method: "POST",
+          uri: "/budgets",
+        }),
+      { wrapper: WrapperHook }
+    );
+    act(() => {
+      result.current.mutate(bodyBudget);
+    });
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+    });
+    expect(result.current.data).toStrictEqual(mockedBudget);
+  });
+
+  it("Patch data", async () => {
+    const { result } = renderHook(
+      () =>
+        useMutateData<Budget, Omit<Partial<Budget>, "id" | "latest">>({
+          key: ["budgets"],
+          method: "PATCH",
+          uri: "/budgets/1",
+        }),
+      { wrapper: WrapperHook }
+    );
+    act(() => {
+      result.current.mutate({
+        category: "General",
+      });
+    });
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+    });
+    expect(result.current.data).toStrictEqual(patchedData);
+  });
+
+  it("Delete data", async () => {
+    const { result } = renderHook(
+      () =>
+        useMutateData({
+          key: ["budgets"],
+          method: "DELETE",
+          uri: "/budgets/1",
+        }),
+      { wrapper: WrapperHook }
+    );
+    act(() => {
+      result.current.mutate(undefined);
+    });
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+    });
+    expect(result.current.data).toStrictEqual({});
+  });
+});
--- src/tests/factories.ts
+++ src/tests/factories.ts
@@ -1,14 +1,6 @@
 import * as Factory from "factory.ts";
-import { Pot, Transaction } from "../types";
+import { Budget, Pot, Transaction } from "../types";
 import { potsColorOptions } from "../components/Pots/constants";
-
-export const PotFactory = Factory.Sync.makeFactory<Partial<Pot>>({
-  id: Factory.each((i) => i + 1),
-  name: Factory.each((i) => `Custom Pot ${i + 1}`),
-  target: Factory.each((i) => (i + 1) * 1000),
-  theme: Factory.each((i) => potsColorOptions[i].value),
-  total: Factory.each((i) => (i + 1) * 100),
-});
 
 export const TransactionFactory = Factory.Sync.makeFactory<Transaction>({
   amount: 100,
@@ -18,4 +10,20 @@ export const TransactionFactory = Factory.Sync.makeFactory<Transaction>({
   id: Factory.each((i) => i + 1),
   name: Factory.each((i) => `Transaction Title ${i + 1}`),
   recurring: false,
+});
+
+export const BudgetFactory = Factory.Sync.makeFactory<Partial<Budget>>({
+  id: Factory.each((i) => i + 1),
+  category: "Category",
+  maximum: 1000,
+  latest: [],
+  theme: "#D1D1D1",
+});
+
+export const PotFactory = Factory.Sync.makeFactory<Partial<Pot>>({
+  id: Factory.each((i) => i + 1),
+  name: Factory.each((i) => `Custom Pot ${i + 1}`),
+  target: Factory.each((i) => (i + 1) * 1000),
+  theme: Factory.each((i) => potsColorOptions[i].value),
+  total: Factory.each((i) => (i + 1) * 100),
 });
