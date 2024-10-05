9672b11d0179b317fc3a0c3d683fba753dde529f
K-ost <golubkovru@gmail.com>
2024-10-05 15:14:12 +0300

    useGetData hook test

---
+++ src/hooks/__test__/useGetData.test.tsx
@@ -0,0 +1,54 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { renderHook, waitFor } from "@testing-library/react";
+import { APINock, WrapperHook } from "../../tests/testUtils";
+import useGetData from "../useGetData";
+import { Transaction } from "../../types";
+import { TransactionFactory } from "../../tests/factories";
+
+const mockedData = TransactionFactory.buildList(3);
+
+describe("useGetData", () => {
+  beforeEach(() => {
+    APINock.get("/transactions").reply(200, mockedData);
+    const { result } = renderHook(
+      () =>
+        useGetData<Transaction[]>({
+          key: ["transactions"],
+          uri: "/transactions",
+          enabled: false,
+        }),
+      { wrapper: WrapperHook }
+    );
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.isSuccess).toBe(false);
+    expect(result.current.data).toBe(undefined);
+  });
+
+  it("useGetData - disabled", async () => {});
+
+  it("useGetData - enabled", async () => {
+    const { result } = renderHook(
+      () =>
+        useGetData<Transaction[]>({
+          key: ["transactions"],
+          uri: "/transactions",
+        }),
+      { wrapper: WrapperHook }
+    );
+
+    expect(result.current.isLoading).toBe(true);
+
+    await waitFor(() => {
+      expect(result.current.isLoading).toBe(false);
+      expect(result.current.isSuccess).toBe(true);
+    });
+
+    expect(result.current.data?.data.length).toBe(3);
+
+    result.current.data?.data.forEach((item) => {
+      expect(item.hasOwnProperty("id")).toBe(true);
+      expect(item.hasOwnProperty("name")).toBe(true);
+      expect(item.hasOwnProperty("amount")).toBe(true);
+    });
+  });
+});
--- src/tests/factories.ts
+++ src/tests/factories.ts
@@ -1,5 +1,5 @@
 import * as Factory from "factory.ts";
-import { Pot } from "../types";
+import { Pot, Transaction } from "../types";
 import { potsColorOptions } from "../components/Pots/constants";
 
 export const PotFactory = Factory.Sync.makeFactory<Partial<Pot>>({
@@ -8,4 +8,14 @@ export const PotFactory = Factory.Sync.makeFactory<Partial<Pot>>({
   target: Factory.each((i) => (i + 1) * 1000),
   theme: Factory.each((i) => potsColorOptions[i].value),
   total: Factory.each((i) => (i + 1) * 100),
+});
+
+export const TransactionFactory = Factory.Sync.makeFactory<Transaction>({
+  amount: 100,
+  avatar: "http://image.link",
+  category: "General",
+  date: new Date("2024-07-09T08:55:27Z"),
+  id: Factory.each((i) => i + 1),
+  name: Factory.each((i) => `Transaction Title ${i + 1}`),
+  recurring: false,
 });
