0c25ef20639f4f705a24a3756b7a10a0a9f2f2b4
K-ost <golubkovru@gmail.com>
2025-03-13 19:47:16 +0200

    balance

--- src/components/BalanceWidget.tsx
+++ src/components/BalanceWidget.tsx
@@ -4,36 +4,43 @@ import { useTranslation } from "react-i18next";
 import useGetData from "../hooks/useGetData";
 import { BalanceType } from "../types";
 import Error from "./Error";
+import { BALANCE_URI } from "../utils/constants";
 
 const BalanceWidget = (): JSX.Element => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const { t } = useTranslation();
 
-  // if (isLoading)
-  //   return (
-  //     <Grid2 container spacing={isMobile ? 3 : 6}>
-  //       {Array.from(new Array(3)).map((__, index) => (
-  //         <Grid2 size={{ xs: 12, sm: 4 }} key={index}>
-  //           <Skeleton height={120} variant="rounded" />
-  //         </Grid2>
-  //       ))}
-  //     </Grid2>
-  //   );
-  // if (isError) return <Error />;
+  const { data, isSuccess, isLoading, isError } = useGetData<BalanceType>({
+    key: ["balance"],
+    uri: BALANCE_URI,
+  });
+
+  if (isLoading)
+    return (
+      <Grid2 container spacing={isMobile ? 3 : 6}>
+        {Array.from(new Array(3)).map((__, index) => (
+          <Grid2 size={{ xs: 12, sm: 4 }} key={index}>
+            <Skeleton height={120} variant="rounded" />
+          </Grid2>
+        ))}
+      </Grid2>
+    );
+
+  if (isError) return <Error />;
 
   return (
     <div>
-      {true && (
+      {isSuccess && (
         <Grid2 container spacing={isMobile ? 3 : 6}>
           <Grid2 size={{ xs: 12, sm: 4 }}>
-            <Balance title={t("balances.current")} amount={1000} dark />
+            <Balance title={t("balances.current")} amount={data.current} dark />
           </Grid2>
           <Grid2 size={{ xs: 12, sm: 4 }}>
-            <Balance title={t("balances.income")} amount={1000} />
+            <Balance title={t("balances.income")} amount={data.income} />
           </Grid2>
           <Grid2 size={{ xs: 12, sm: 4 }}>
-            <Balance title={t("balances.expenses")} amount={1000} />
+            <Balance title={t("balances.expenses")} amount={data.expenses} />
           </Grid2>
         </Grid2>
       )}
