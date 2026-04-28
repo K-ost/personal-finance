9f6a29904e0a9ca908024dcd3ef91c94e73c6174
K-ost <golubkovru@gmail.com>
2026-04-28 21:58:45 +0300

    fixing constant url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
 
 export const BALANCE_URI = "/balance";
 export const TRANSACTIONS_URI = "/transactions";
