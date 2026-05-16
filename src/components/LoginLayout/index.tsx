caf99fda75b665f0f4d15da1db867ad2efcfba38
K-ost <golubkovru@gmail.com>
2026-05-16 16:38:09 +0300

    changing LoginLayout, removing MUI styles

--- src/components/LoginLayout/index.tsx
+++ src/components/LoginLayout/index.tsx
@@ -1,10 +1,9 @@
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
 import bg from "../../assets/illustration-authentication.svg";
 import logo from "../../assets/logo-large.svg";
 import LangSwitcher from "../LangSwitcher";
-import { Layout, LeftCol, LeftColText, RightCol } from "./styles";
 
 type LoginLayoutProps = {
   children: React.ReactNode;
@@ -14,19 +13,23 @@ const LoginLayout = (props: LoginLayoutProps): JSX.Element => {
   const { children } = props;
   const { t } = useTranslation();
 
+  const leftColClass =
+    "bg-[#201F24] rounded-bl-xl rounded-br-xl text-white flex flex-col m-0 md:mr-5 overflow-hidden p-6 md:p-10 relative w-auto md:w-110 lg:w-140 md:rounded-xl";
+
   return (
-    <Layout>
-      <LeftCol>
-        <img src={bg} alt="" className="bg" />
-        <Box sx={{ position: "relative", flexGrow: 1 }}>
+    <div className="bg-beige-100 flex h-full p-0 md:p-5 flex-col md:flex-row">
+      <div className={leftColClass}>
+        <img src={bg} alt="" className="hidden absolute w-full left-0 top-0 md:block" />
+        <div className="relative flex-1">
           <img src={logo} alt="" />
-        </Box>
-        <LeftColText>
+        </div>
+        <div className="hidden md:block relative">
           <Typography variant="h1">{t("loginPage.text")}</Typography>
           <Typography variant="body1">{t("loginPage.subtext")}</Typography>
-        </LeftColText>
-      </LeftCol>
-      <RightCol>
+        </div>
+      </div>
+
+      <div className="p-4 md:p-0 flex items-center justify-center grow">
         <LangSwitcher
           sx={(theme) => ({
             position: "absolute",
@@ -38,8 +41,8 @@ const LoginLayout = (props: LoginLayoutProps): JSX.Element => {
           })}
         />
         {children}
-      </RightCol>
-    </Layout>
+      </div>
+    </div>
   );
 };
 
--- src/ui/FormBody.tsx
+++ src/ui/FormBody.tsx
@@ -1,23 +1,12 @@
-import { Box, BoxProps, styled } from "@mui/material";
-
 type FormBodyrops = {
   children: React.ReactNode;
 };
 
-const Div = styled(Box)<BoxProps>(({ theme }) => ({
-  backgroundColor: theme.palette.background.paper,
-  borderRadius: 12,
-  maxWidth: 560,
-  padding: theme.spacing(8),
-  width: "100%",
-  [theme.breakpoints.down("md")]: {
-    padding: `${theme.spacing(6)} ${theme.spacing(5)}`,
-  },
-}));
-
 const FormBody = (props: FormBodyrops) => {
   const { children } = props;
-  return <Div>{children}</Div>;
+  return (
+    <div className="bg-white rounded-2xl w-full max-w-140 p-6 md:p-8">{children}</div>
+  );
 };
 
 export default FormBody;
